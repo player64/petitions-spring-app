b3b7c6896a049a3bcdd27e73a876ad3876b1a773
player64 <player.c64@gmail.com>
2024-11-02 12:44:41 +0000

    Repository initialised with Spring Boot configuration

---
+++ src/main/java/org/player64/mariuszspetitions/MariuszsPetitionsApplication.java
@@ -0,0 +1,13 @@
+package org.player64.mariuszspetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MariuszsPetitionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MariuszsPetitionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/player64/mariuszspetitions/MariuszsPetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.player64.mariuszspetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariuszsPetitionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
